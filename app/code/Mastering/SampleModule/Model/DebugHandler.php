411e415995f19a37016714ed309cab57fb0c501f
isabellaveronezi <isabellavero96@icloud.com>
2019-09-20 00:49:04 -0300

    finishing module

--- app/code/Mastering/SampleModule/Model/DebugHandler.php
+++ app/code/Mastering/SampleModule/Model/DebugHandler.php
@@ -2,19 +2,18 @@
 
 namespace Mastering\SampleModule\Model;
 
-use Monolog\Logger;
 use Magento\Framework\Logger\Handler\Base;
+use Monolog\Logger;
 
-class Debug extends Base
+class DebugHandler extends Base
 {
     /**
      * @var string
      */
-    protected $fileName = '/var/log/debug.log';
+    protected $fileName = '/var/log/debug_custom.log';
 
     /**
      * @var int
      */
     protected $loggerType = Logger::DEBUG;
 }
-
